ef33b75e07dffbc52dd6813927a947df0d060673
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-06-13 11:44:35 +0100

    Load PDF

--- rag.py
+++ rag.py
@@ -1,6 +1,15 @@
 import dotenv
 import os
+from langchain_community.document_loaders import PyPDFLoader
+
+
 dotenv.load_dotenv()
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 
+file_path = "PDFs\Metric Ensembles for Hallucination Detection.pdf"
+loader = PyPDFLoader(file_path)
+
+docs = loader.load()
+
+print(len(docs))
